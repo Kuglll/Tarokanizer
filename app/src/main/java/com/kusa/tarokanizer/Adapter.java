475d6feba09d8e054a3105e90b137d29e27cc778
Kuglll <mark.frelih@gmail.com>
2020-04-20 20:36:10 +0200

    Make MainActivity nicer

--- app/src/main/java/com/kusa/tarokanizer/Adapter.java
+++ app/src/main/java/com/kusa/tarokanizer/Adapter.java
@@ -5,17 +5,17 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.ImageView;
-
-import androidx.annotation.NonNull;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.recyclerview.widget.RecyclerView;
+import android.widget.TextView;
 
 import com.kusa.tarokanizer.data_classes.CardView;
 import com.kusa.tarokanizer.data_classes.Settings;
 
 import java.util.ArrayList;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
@@ -47,7 +47,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
             super(itemView);
             title = itemView.findViewById(R.id.title);
             mDeleteImage = itemView.findViewById(R.id.image_delete);
-            constraintLayout = itemView.findViewById(R.id.parent_layout);
+            constraintLayout = itemView.findViewById(R.id.item);
 
             mDeleteImage.setOnClickListener(new View.OnClickListener() {
                 @Override
